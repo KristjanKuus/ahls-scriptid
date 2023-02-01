ab1588a0ed0d2626b5fa86067f9099e5ecc7d879
KristjanKuus <74591012+KristjanKuus@users.noreply.github.com>
2023-02-01 17:16:52 +0200

    Update mysql_server.sh

--- mysql_server.sh
+++ mysql_server.sh
@@ -1,5 +1,7 @@
+#Vaatab, kas MySql juba eksisteerib
 MYSQL=$(dpkg-query -W -f='${Status)' mysql-server 2>/dev/null | grep -c 'ok installed')
 if [ $MYSQL -eq 0 ];
+#Kui eksisteeri, paigaldada
   then
   echo "Mysql'i paigaldus koos vajalikute lisadega"
   apt install mysql-server
@@ -9,6 +11,7 @@ if [ $MYSQL -eq 0 ];
   echo "host=localhost" >> $HOME/.my.cnf
   echo "user = root" >> $HOME/.my.cnf echo "password = qwerty" >> $HOME/.my.cnf
 elif [ $MYSQL -eq 1 ];
+#MySql on paigaldatud.
   then
   echo "Mysql Server on juba varem paigaldatud"
   mysql
