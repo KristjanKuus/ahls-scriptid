bdb43aff712912eb97362214f1096d92fac4448c
KristjanKuus <74591012+KristjanKuus@users.noreply.github.com>
2023-02-01 17:18:03 +0200

    Update php_paigaldus.sh

--- php_paigaldus.sh
+++ php_paigaldus.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#Paigaldab Apache
-
 file=/var/www/html/index.html
 user=$(whoami)
+#Vaatab, kas PHP juba eksisteerib
 php=$(dpkg-query -W -f='${Status}' php 2>/dev/null | grep -c 'ok installed')
 if [ $php -eq 0 ]
 then
+#Paigaldab PHP
 	echo "Alustan php paigaldamise"
 	apt install php7.0 libapache2-mod-php7.0 php7.0-mysql
 	echo "Php on paigaldatud"
 elif [ $php -eq 1 ]
+#PHP on paigaldatud.
 then
 	echo "Php on juba paigaldatud"
 	systemctl start php
