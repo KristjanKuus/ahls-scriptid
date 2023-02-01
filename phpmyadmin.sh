985922d07cdcf86cbf05fa84bd408a726a9970b7
KristjanKuus <74591012+KristjanKuus@users.noreply.github.com>
2023-02-01 17:16:03 +0200

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,10 +1,13 @@
+#Vaatab, kas PhpMyAdmin juba eksisteerib
 PMA=$(dpkg-query -W -f='${Status)' phpmyadmin 2>/dev/null | grep -c 'ok installed')
 if [ $PMA -eq 0 ];
+#Kui eksisteeri, paigaldada
   then
   echo "PhpMyAdmin'i paigaldus koos vajalikute lisadega"
   apt install phpmyadmin
   echo "PhpMyAdmin on nüüd paigaldatud"
-elif [ $PMA -eq 1 ]; 
+elif [ $PMA -eq 1 ];
+#PhpMyAdmin on paigaldatud.
   then
   echo "PhpMyAdmin on juba varem paigaldatud"
 fi
